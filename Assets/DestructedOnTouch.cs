5bf08a7dd2036a62e6e3d46e7e0fb029728b245e
Vadim Dol <Dolvadim93@gmail.com>
2019-12-01 22:07:36 +0200

    Pushed more then 100 files! muhahaha

---
+++ Assets/DestructedOnTouch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestructedOnTouch : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        int score =++player.GetComponent<ScoreKeeper>().score;
+        Debug.Log(score);
+        Destroy(this.gameObject,0);
+        if (score >= 3) {
+            SceneManager.LoadScene("WinScreen");
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
